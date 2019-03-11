656840b8f5b4cb26954b6ccf28b89c41bdaac5ab
Natte Sharan <nattesharan@gmail.com>
2019-03-12 00:12:48 +0530

    added html template

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello It workkksssss !")
+    return render(request, 'accounts/login.html')
