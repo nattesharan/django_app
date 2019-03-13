b786c858d7fbdf391ed466d1c803aae6469fa3d4
Natte Sharan <nattesharan@gmail.com>
2019-03-13 22:46:26 +0530

    simple login form using django builtin login view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
+from django.contrib.auth.views import login
 from . import views
 urlpatterns = [
-    url(r'^$', views.home)
+    url(r'^$', views.home),
+    url(r'^login/$', login, {'template_name': 'accounts/login.html'})
 ]
