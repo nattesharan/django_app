45f510e902f0b300bf37f677201d508d3c630fe0
Natte Sharan <nattesharan@gmail.com>
2019-03-14 01:09:37 +0530

    added signals

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinLengthValidator
+from django.db.models.signals import post_save
 # Create your models here.
 
 class UserProfile(models.Model):
@@ -9,3 +10,13 @@ class UserProfile(models.Model):
     city = models.CharField(max_length = 32,default='')
     website = models.URLField(default='')
     phone = models.CharField(max_length=10, validators=[MinLengthValidator(10)])
+
+    def __str__(self):
+        return self.user.username
+        
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+
+post_save.connect(create_profile, sender=User)
