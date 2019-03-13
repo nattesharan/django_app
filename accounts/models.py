06bfe9a34698080abcfaa29bfa72fb6d2bf96e07
Natte Sharan <nattesharan@gmail.com>
2019-03-14 01:02:42 +0530

    added userprofile django model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='profile')
+    description = models.CharField(max_length=256, default='')
+    city = models.CharField(max_length = 32,default='')
+    website = models.URLField(default='')
+    phone = models.CharField(max_length=10, validators=[MinLengthValidator(10)])
