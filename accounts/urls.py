96b9c7757b98e2aa9c498b97a5434863ba376928
Natte Sharan <nattesharan@gmail.com>
2019-03-30 03:06:22 +0530

    remove hardcoded urls from templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,7 +11,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name="home"),
     url(r'^login/$', login, {'template_name': 'accounts/login.html'}, name="login"),
-    url(r'^logout/$', views.logout_user),
+    url(r'^logout/$', views.logout_user, name="logout"),
     url(r'^register/$', views.register, name='register'),
     url(r'^profile/$', views.view_profile, name='view_profile'),
     url(r'^profile/edit/$', views.edit_profile, name='edit_profile'),
