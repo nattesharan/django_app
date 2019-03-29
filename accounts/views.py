75a42871aa3690c352ad4b29e72bd6c05f5e43b5
Natte Sharan <nattesharan@gmail.com>
2019-03-30 01:37:23 +0530

    added login required decorator and changes to nav bar

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect, reverse
 from accounts.forms import RegistrationForm, EditProfileForm
 from django.contrib.auth import logout, update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required
 def home(request):
     numbers = [1,2,3,4,5]
     name = "Sharan"
@@ -30,16 +32,19 @@ def register(request):
         }
         return render(request, 'accounts/register.html',context)
 
+@login_required
 def logout_user(request):
     logout(request)
     return redirect(reverse('main'))
 
+@login_required
 def view_profile(request):
     context = {
         'user': request.user
     }
     return render(request, 'accounts/profile.html', context=context)
 
+@login_required
 def edit_profile(request):
     if request.method == 'POST':
         form = EditProfileForm(request.POST, instance = request.user)
@@ -50,6 +55,7 @@ def edit_profile(request):
         form = EditProfileForm(instance = request.user)
     return render(request, 'accounts/edit_profile.html', {'form': form})
 
+@login_required
 def change_password(request):
     if request.method == 'POST':
         form = PasswordChangeForm(data=request.POST, user=request.user)
