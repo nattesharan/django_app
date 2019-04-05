40101729aeca36b7356e62d4db0eef96ba558d90
Natte Sharan <nattesharan@gmail.com>
2019-04-06 00:32:09 +0530

    chage sort order of queryset in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -10,5 +10,9 @@ class UserProfileAdmin(admin.ModelAdmin):
     def user_info(self, instance):
         return instance.description
     
+    def get_queryset(self, request):
+        queryset = super(UserProfileAdmin, self).get_queryset(request)
+        return queryset.order_by('-phone', 'user')
+    
     user_info.short_description = 'Description'
 admin.site.register(models.UserProfile, UserProfileAdmin)
