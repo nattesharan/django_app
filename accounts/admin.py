88ebee844ff4eef11760943f23af694d4b05eef2
Natte Sharan <nattesharan@gmail.com>
2019-04-05 21:16:57 +0530

    add more model coulmns in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from . import models
 # Register your models here.
-admin.site.register(models.UserProfile)
+# admin.site.register(models.UserProfile)
 # admin.site.site_header = 'My Administration'
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone')
+
+admin.site.register(models.UserProfile, UserProfileAdmin)
