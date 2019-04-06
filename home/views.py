bfd94e5ce75f3fffe10efed00a085c7086f6f93f
Natte Sharan <nattesharan@gmail.com>
2019-04-06 14:58:25 +0530

    handle post rquest in class based view

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from home.forms import HomeForm
 # Create your views here.
@@ -10,4 +10,11 @@ class HomeView(TemplateView):
 
     def get(self, request):
         form = HomeForm()
+        return render(request, self.template_name, {'form': form})
+    
+    def post(self, request):
+        form = HomeForm(request.POST)
+        if form.is_valid():
+            post = form.cleaned_data['post']
+            return redirect('home:home')
         return render(request, self.template_name, {'form': form})
