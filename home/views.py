c1c57cc7ed1fc10a9d5c453bcee2fe58f5b0defb
Natte Sharan <nattesharan@gmail.com>
2019-04-06 14:46:07 +0530

    added django form in home

--- home/views.py
+++ home/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+from home.forms import HomeForm
 # Create your views here.
 def home(request):
     return render(request, 'home/home.html')
 
 class HomeView(TemplateView):
     template_name = 'home/home.html'
+
+    def get(self, request):
+        form = HomeForm()
+        return render(request, self.template_name, {'form': form})
