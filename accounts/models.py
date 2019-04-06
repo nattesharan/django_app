342838932e26a63100b09f7bb7e3d3ee52a6e395
Natte Sharan <nattesharan@gmail.com>
2019-04-06 12:59:36 +0530

    added sample model manager

--- accounts/models.py
+++ accounts/models.py
@@ -2,8 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinLengthValidator
 from django.db.models.signals import post_save
-# Create your models here.
 
+class UserProfileManager(models.Manager):
+    def get_queryset(self):
+        return super(UserProfileManager, self).get_queryset().filter(user__is_superuser=True)
+
+# Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name='profile')
     description = models.CharField(max_length=256, default='')
@@ -11,7 +15,8 @@ class UserProfile(models.Model):
     website = models.URLField(default='')
     phone = models.CharField(max_length=10, validators=[MinLengthValidator(10)])
     image = models.ImageField(upload_to='profile_images', blank=True)
-
+    objects = models.Manager()
+    super_user_profile = UserProfileManager()
     def __str__(self):
         return self.user.username
         
