ca80573a799e6af602b209404787715c333ee49d
Natte Sharan <nattesharan@gmail.com>
2019-04-09 00:35:05 +0530

    added attrs to wiget

--- home/forms.py
+++ home/forms.py
@@ -5,4 +5,9 @@ class HomeForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('post',)
-    post= forms.CharField(required=True)
+    post= forms.CharField(required=True,widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Post Something !!'
+        }
+    ))
