f649c86c67fd4989aa19f7ace1d173f6603f71bb
Natte Sharan <nattesharan@gmail.com>
2019-04-27 20:30:10 +0530

    added commnts on serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,17 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from home.models import Post
 
+
+'''
+serializers.Serializer:
+Normal serializer means we need to create our own fields and validations 
+UseCase: If we want to send a different response to the user or get differnt body from the request in this case
+its better we use THis
+serializers.ModelSerializer:
+When we have a model and we need to perform basic crud on the model in this case we use ModelSerializer
+
+'''
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -9,4 +20,13 @@ class UserSerializer(serializers.ModelSerializer):
             'first_name',
             'last_name',
             'username'
+        )
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = (
+            'post',
+            'user',
+            'created_on'
         )
--- home/models.py
+++ home/models.py
@@ -10,3 +10,17 @@ class Post(models.Model):
 
     def __str__(self):
         return self.post
+
+'''
+on_delete behaviour:
+This is the behaviour to adopt when the referenced object is deleted. It is not specific to django, this is an SQL standard.
+
+There are 6 possible actions to take when such event occurs:
+
+CASCADE: When the referenced object is deleted, also delete the objects that have references to it (When you remove a blog post for instance, you might want to delete comments as well). SQL equivalent: CASCADE.
+PROTECT: Forbid the deletion of the referenced object. To delete it you will have to delete all objects that reference it manually. SQL equivalent: RESTRICT.
+SET_NULL: Set the reference to NULL (requires the field to be nullable). For instance, when you delete a User, you might want to keep the comments he posted on blog posts, but say it was posted by an anonymous (or deleted) user. SQL equivalent: SET NULL.
+SET_DEFAULT: Set the default value. SQL equivalent: SET DEFAULT.
+SET(...): Set a given value. This one is not part of the SQL standard and is entirely handled by Django.
+DO_NOTHING: Probably a very bad idea since this would create integrity issues in your database (referencing an object that actually doesn't exist). SQL equivalent: NO ACTION
+'''
