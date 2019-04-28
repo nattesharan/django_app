dc102c70a98df630a3dd77522f3acdd2eff5e985
Natte Sharan <nattesharan@gmail.com>
2019-04-28 18:37:46 +0530

    added class based apis

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 from django.conf.urls import url
-from api.views import UserView, posts, post_detail
+from api.views import UserView, posts, post_detail, PostsApiView
 
 router = routers.DefaultRouter()
 
@@ -9,5 +9,8 @@ router.register(r'^users', UserView)
 urlpatterns = router.urls
 urlpatterns += [
     url(r'^posts/$', posts, name='posts'),
-    url(r'^posts/(?P<pk>\d+)/$', post_detail, name='post_detail')
+    url(r'^posts/(?P<pk>\d+)/$', post_detail, name='post_detail'),
+    # I dont want to have multiple classess for handling details and list separately so I'll change the route accordingly
+    url(r'^cposts/$', PostsApiView.as_view(), name='class_posts_api'),
+    url(r'^cposts/(?P<pk>\d+)/$', PostsApiView.as_view(), name='class_post_details_api')
 ]
--- api/views.py
+++ api/views.py
@@ -6,10 +6,63 @@ from django.http import JsonResponse
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
+# we use view sets when we want a view or we want all the crud to be implemented without any other operations
 class UserView(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class PostsApiView(APIView):
+    # the main advantage of using class over functional view is no need to worry about parsing !!!! Hurrayyyyy !!! 
+    def get(self, request, pk=None):
+        posts = Post.objects.all()
+        if pk:
+            try:
+                post = posts.get(pk=pk)
+            except ObjectDoesNotExist:
+                return Response({'error': 'Post not found with {}'.format(pk)}, status=404)
+            post = PostSerializer(post)
+            return Response(post.data)
+        posts = Post.objects.all()
+        posts = PostSerializer(posts, many=True)
+        return Response(posts.data)
+
+    def post(self, request, pk=None):
+        if pk:
+            return Response({'error': 'Unhandled Method !!'}, status=403)
+        # there is no need for us to do parsing as APIView does it for us !! Thanks APIView.....
+        data = request.data
+        post = PostSerializer(data=data)
+        if post.is_valid():
+            post.save()
+            return Response(post.data, status=201)
+        return Response(post.errors, status=400)
+    
+    def put(self, request, pk=None):
+        if not pk:
+            return Response({'error': 'Unhandled Method !!'}, status=403)
+        try:
+            post = Post.objects.get(pk=pk)
+        except ObjectDoesNotExist:
+            return JsonResponse({'error': 'Post not found with {}'.format(pk)}, status=404)
+        data = request.data
+        post = PostSerializer(post, data=data)
+        if post.is_valid():
+            post.save()
+            return Response(post.data)
+    
+    def delete(self, request, pk=None):
+        if not pk:
+            return Response({'error': 'Unhandled Method !!'}, status=403)
+        try:
+            post = Post.objects.get(pk=pk)
+        except ObjectDoesNotExist:
+            return JsonResponse({'error': 'Post not found with {}'.format(pk)}, status=404)
+        post.delete()
+        return Response({'message': 'Successfully deleted the post !'}, status=204)
+
 
 @csrf_exempt
 def posts(request):
