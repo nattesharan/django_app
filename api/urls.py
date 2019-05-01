cb95c4e286554f77ad528f94a31d9864cf7d3381
Natte Sharan <nattesharan@gmail.com>
2019-05-01 12:18:44 +0530

    added token auth

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from rest_framework import routers
 from django.conf.urls import url
 from api.views import UserView, posts, post_detail, PostsApiView, PostsGenericView
+from rest_framework.authtoken.views import ObtainAuthToken
 
 router = routers.DefaultRouter()
 
@@ -14,5 +15,6 @@ urlpatterns += [
     url(r'^cposts/$', PostsApiView.as_view(), name='class_posts_api'),
     url(r'^cposts/(?P<pk>\d+)/$', PostsApiView.as_view(), name='class_post_details_api'),
     url(r'^gposts/$', PostsGenericView.as_view(), name='generic_posts_get'),
-    url(r'^gposts/(?P<pk>\d+)/$', PostsGenericView.as_view(), name='generic_post_detail')
+    url(r'^gposts/(?P<pk>\d+)/$', PostsGenericView.as_view(), name='generic_post_detail'),
+    url(r'^auth/login/$',ObtainAuthToken.as_view(), name='auth_token_view')
 ]
--- django_app/settings.py
+++ django_app/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
