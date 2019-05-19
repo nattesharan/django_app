7d135895261264f968b608e297095d0fe0cc858d
Natte Sharan <nattesharan@gmail.com>
2019-05-19 12:00:12 +0530

    added advanced filters by using custom filter class

--- api/custom_filters.py
+++ api/custom_filters.py
@@ -9,9 +9,17 @@ class UserFilter(FilterSet):
     pk = NumberFilter('id') # /?id=1
     is_active = CharFilter('is_active') # /?is_active=Trues
     profile = NumberFilter('profile__id') # /?profile=1
+    min_age = NumberFilter(method='filter_by_min_age')
+    max_age = NumberFilter(method='filter_by_max_age')
     
     class Meta:
         model = User
         # we ddeclare the filter names here like ?username=tesr so username can be created as filter
         # the fields can be named anything but they need to be mapped with the correct field in the model
         fields = ('pk', 'is_active', 'profile')
+    
+    def filter_by_min_age(self, queryset, filter_name, value):
+        return queryset.filter(profile__age__gte=value)
+    
+    def filter_by_max_age(self, queryset, filter_name, value):
+        return queryset.filter(profile__age__lte=value)
