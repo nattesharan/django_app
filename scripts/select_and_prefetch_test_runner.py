b07c592c4a1ba0e8f89b1cd98a775955b1e90aba
Natte Sharan <nattesharan@gmail.com>
2019-05-26 22:49:37 +0530

    get the query info of a function

--- scripts/select_and_prefetch_test_runner.py
+++ scripts/select_and_prefetch_test_runner.py
@@ -1,4 +1,5 @@
 from extra_topics.utils import list_products
+from extra_topics.decorators import query_info
 
 def run():
     '''
@@ -7,4 +8,21 @@ def run():
     products_list = list_products()
     # We have all our products, but what’s the problem with this function?
     # Let’s see how many queries this function does into the database:
+    '''
+    I created one debugger decorator to see how many queries each util function takes, 
+    it shows the number of the queries and the time in seconds for each example
+    '''
+    query_info(list_products)()
+
+    '''
+    Wow! 501 queries for that simple piece of code?? What happened?
+
+    For each time we access one foreign key that is not in cache, another
+    query will be made to retrieve the value.
+
+    In our case, we accessing the category inside a loop, one query will be made for each time the loop executes.
     
+    To avoid this high number of queries when accessing foreign keys or one to one fields we can use the select_related 
+    method.
+    '''
+
