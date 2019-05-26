0b382b7129cc2fbfc573196e1b65d2fb4013f750
Natte Sharan <nattesharan@gmail.com>
2019-05-26 20:16:31 +0530

    added django extra_topics app which can be used to  demonstrate any basic topics

---
+++ extra_topics/__init__.py
@@ -0,0 +1,3 @@
+# This module exclusively consists of any additional topics
+# as for each topic I'll need to create a new model and as I cant
+# write api for each and everything so ill just expalin them here
---
+++ extra_topics/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ExtraTopicsConfig(AppConfig):
+    name = 'extra_topics'
