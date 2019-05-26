166c69c66cc4ea0b3578c1f76a28044e8ba942a0
Natte Sharan <nattesharan@gmail.com>
2019-05-26 22:31:55 +0530

    added description

--- extra_topics/__init__.py
+++ extra_topics/__init__.py
@@ -6,5 +6,6 @@
 Topics List
 _________________
 
-1) select_related and prefetch_related
+1) select_related and prefetch_related: I will be demonstrating this using Product and Category models. The Product has 
+a foreign key relationship with the Category and the Category has one many to many relationships with itself.
 '''
