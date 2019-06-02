f071a8690997a8f841fe38000b9279462d10e807
Natte Sharan <nattesharan@gmail.com>
2019-06-02 13:44:35 +0530

    illustrate prefetch_related

--- extra_topics/utils.py
+++ extra_topics/utils.py
@@ -10,6 +10,21 @@ def get_products_info(products_qs):
         })
     return products
 
+def get_categories_info(categories_qs, active_subcategories=False):
+    categories = []
+    for category in categories_qs:
+        if active_subcategories:
+            subcategories = [sub.name for sub in category.sub_categories.filter(is_active=True)]
+        else:
+            subcategories = [sub.name for sub in category.sub_categories.all()]
+        categories.append({
+            'name': category.name,
+            'is_active': category.is_active,
+            'subcategories': subcategories
+        })
+    return categories
+
+
 def list_products():
     products_qs = Product.objects.all()
     products = get_products_info(products_qs)
@@ -20,3 +35,17 @@ def list_products_with_select_related():
     products = get_products_info(products_qs)
     return products
 
+def list_categories():
+    categories_qs = Category.objects.all()
+    categories = get_categories_info(categories_qs)
+    return categories
+
+def list_categories_with_prefetch_related():
+    categories_qs = Category.objects.prefetch_related('sub_categories').all()
+    categories = get_categories_info(categories_qs)
+    return categories
+
+def categories_list_active_subcategories():
+    """Return all categories and list only the active subcatories."""
+    categories_qs = Category.objects.prefetch_related("sub_categories")
+    categories = get_categories_info(categories_qs, active_subcategories=True)
--- scripts/select_and_prefetch_test_runner.py
+++ scripts/select_and_prefetch_test_runner.py
@@ -1,16 +1,20 @@
-from extra_topics.utils import list_products, list_products_with_select_related
+from extra_topics.utils import (list_products, list_products_with_select_related, 
+                                list_categories, list_categories_with_prefetch_related,
+                                categories_list_active_subcategories)
 from extra_topics.decorators import query_info
 
 def run():
     '''
     This executes all the functions in an order to explain how select_related and prefetch_related works
     '''
+    # 1) Select related isslustration
     products_list = list_products()
     # We have all our products, but what’s the problem with this function?
     # Let’s see how many queries this function does into the database:
     '''
     I created one debugger decorator to see how many queries each util function takes, 
     it shows the number of the queries and the time in seconds for each example
+    select_related is used for feoreign keys or one to many fields or one to one relation ships
     '''
     query_info(list_products)()
     print("#######################################################")
@@ -31,6 +35,48 @@ def run():
     assert products_fetched_with_select_related == products_list
 
     query_info(list_products_with_select_related)()
-
+    print("#######################################################")
     # Congrats! Now we did only 1 query instead of 501, it’s a real improvement
+
+    # 2) Prefetch related illustration
+    # prefetch related is used for many to many relationships
+    categories_list = list_categories()
+
+    query_info(list_categories)()
+    print("#######################################################")
+    '''
+    The above funtion took 51 queries to fetch the data
+    '''
+    categories_fetched_with_prefetch_related = list_categories_with_prefetch_related()
+
+    assert categories_fetched_with_prefetch_related == categories_list
+
+    query_info(list_categories_with_prefetch_related)()
+    '''
+    why we have two queries?
+    the prefetch_related made the JOIN using Python rather than in the database.
+    In this case, Django made two queries and then join the results into one queryset for us.
+    '''
+    print("#######################################################")
+
+    '''
+    Let’s create a new function that will return only the active subcategories using the prefetch_related
+    '''
+
+    query_info(categories_list_active_subcategories)()
+    print("#######################################################")
+    '''
+    WOW! We increased the number of queries, how it’s possible?????
+    Calm down, there is a reason for this:
+    When we use the prefetch related we are saying to Django we want all the results to be JOINED, but when we use the 
+    filter(is_active=True) we are changing the primary query and then Django doesn’t JOIN the right result
+    That’s why we have 52 queries, 51 queries iterating over the categories and 1 query to get all the results in prefetch.s for us.
+    '''
+    # Let’s see how to solve this problem using the new Prefetch introduced recently by Django
+    
+
+
+
+
+
 
