c0cd10891c56d5cb36275be0d478f261dc8cbe94
Natte Sharan <nattesharan@gmail.com>
2019-08-01 22:24:57 +0530

    changes

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -17,7 +17,7 @@ source venv/bin/activate
 pip install -r requirements.txt
 sudo sonar-scanner -Dsonar.projectKey=django_app -Dsonar.sources=. -Dsonar.login=77208f48bdbff3b1855ccfe3db2d24823e6596fe
 python manage.py migrate
-sudo venv/bin/celery multi restart django_app -A django_app --pidfile="%n.pid" --loglevel=info --logfile='django_app_celery.log'
-sudo venv/bin/celery multi restart django_app_beat -A django_app --beat -c 1 --pidfile="%n.pid" --loglevel=info logfile="django_beat.log" --scheduler=django_celery_beat.schedulers:DatabaseScheduler
+sudo venv/bin/celery multi restart django_app -A django_app --pidfile="django_app_celery.pid" --loglevel=info --logfile='django_app_celery.log'
+sudo venv/bin/celery multi restart django_app_beat -A django_app --beat -c 1 --pidfile="django_app_celery_beat.pid" --loglevel=info logfile="django_beat.log" --scheduler=django_celery_beat.schedulers:DatabaseScheduler
 sudo supervisorctl restart django_app
 whoami
